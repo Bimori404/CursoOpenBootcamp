91f94e786608340097993c656d0c9c5d66775d34
Jordanius <106877704+JordaniusNotDev@users.noreply.github.com>
2022-12-13 14:15:06 -0600

    Add files via upload

---
+++ EjerciciosTema3/Ejercicio2/Coche.java
@@ -0,0 +1,10 @@
+package EjerciciosTema3.Ejercicio2;
+
+public class Coche {
+    int numPuertas; // creamos la variable para las puertas del coche
+
+    public void aumentarNumPuertas() {
+        this.numPuertas++; // le aumentamos una puerta
+    }
+    
+}
